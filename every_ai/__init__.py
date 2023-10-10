1fb801b85045d9946e36d1d78770c2376241fbd5
Tom Usher <tom@tomusher.com>
2023-10-10 15:14:07 +0000

    Move backend import location

--- every_ai/__init__.py
+++ every_ai/__init__.py
@@ -1,6 +1,8 @@
 from .backends import registry
+from .backends.anthropic import AnthropicBackend  # noqa: F401
 from .backends.base import AIBackend
 from .backends.exceptions import InvalidAIBackendError
+from .backends.openai import OpenAIBackend  # noqa: F401
 
 VERSION = (1, 0, 0)
 __version__ = ".".join(map(str, VERSION))
--- every_ai/backends/__init__.py
+++ every_ai/backends/__init__.py
@@ -1,4 +1,2 @@
-from .anthropic import AnthropicBackend  # noqa: F401
 from .base import AIBackend, ChatAbility, EmbeddingAbility  # noqa: F401
-from .openai import OpenAIBackend  # noqa: F401
 from .registry import _registry as registry  # noqa: F401
